51adf697bf0e3f187ad2f59b8c673a07d6fe51bd
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-14 21:17:16 +0300

    product app was created

---
+++ product/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Product
+
+# readonly_fields=("create_date",)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_editable = ( "is_in_stock", )
+
+
+
+
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+admin.site.site_title = "Clarusway Title"
+admin.site.site_header = "Clarusway Admin Portal"  
+admin.site.index_title = "Welcome to Clarusway Admin Portal"
