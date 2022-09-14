b6f5345deeeb5bf4ad30cd4841abeed95e7cbda6
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-14 21:35:42 +0300

    admin panel developped

--- product/admin.py
+++ product/admin.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
 from .models import Product
 
-# readonly_fields=("create_date",)
+
 
 
 class ProductAdmin(admin.ModelAdmin):
+    # readonly_fields=("create_date",)
     list_display = ("name", "create_date", "is_in_stock", "update_date")
     list_editable = ( "is_in_stock", )
+    list_display_links = ("create_date", )
+    search_fields = ("name", "create_date")
+    prepopulated_fields = {'slug' : ('name',)}
+    list_per_page = 10
+    ordering = ("name",)
+    date_hierarchy = "update_date"
+    # fields = (('name', 'slug'), 'description', "is_in_stock")
+    fieldsets = (
+        ("General fields", {
+            "fields": (
+                ('name', 'slug'), "is_in_stock" # to display multiple fields on the same line, wrap those fields in their own tuple
+            ),
+            # 'classes': ('wide', 'extrapretty'), wide or collapse
+        }),
+        ('Optionals Settings', {
+            # "classes" : ("collapse", ),
+            "fields" : ("description",),
+            'description' : "You can use this section for optionals settings"
+        })
+    )
+
 
 
 
