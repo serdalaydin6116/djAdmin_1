b5b6e72b526b7f2c0e028ffa641e0c74543b7bcf
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-15 22:29:42 +0300

    models was updated

--- product/models.py
+++ product/models.py
@@ -3,9 +3,7 @@ from ckeditor.fields import RichTextField
 from django.utils.safestring import mark_safe
 
 
-
-
-
+# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="category name")
     is_active = models.BooleanField(default=True)
@@ -15,38 +13,31 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
-
-
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     # description = models.TextField(blank=True, null=True)
     description = RichTextField()
-    product_img = models.ImageField(null=True, blank=True, default="defaults/clarusway.png", upload_to="product/")
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
     is_in_stock = models.BooleanField(default=True)
     slug = models.SlugField(null=True, blank=True)
     categories = models.ManyToManyField(Category, related_name="products")
-
+    product_img = models.ImageField(null=True, blank=True, default="defaults/clarusway.png", upload_to="product/")
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
     
     def __str__(self):
         return self.name
-
-
+    
     def how_many_reviews(self):
         count = self.reviews.count()
         return count
-
-
-    def bring_image(self):
-        if self.product_img:
-            return mark_safe(f"<img src={self.product_img.url} width=400 height=400></img>")
-        return mark_safe(f"<h3>{self.name} has not image </h3>")
-
+    
+    # def bring_image(self):
+    #     if self.product_img:
+    #         return mark_safe(f"<img src={self.product_img.url} width=400 height=400></img>")
+    #     return mark_safe(f"<h3>{self.name} has not image </h3>")
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
