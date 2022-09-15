2d8f9b0afa20f74b9bd4ed44132e8293c648f221
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-15 22:36:19 +0300

    filter, imp-exp were added

--- product/admin.py
+++ product/admin.py
@@ -4,7 +4,9 @@ from django.utils.safestring import mark_safe
 from .models import Category, Product,Review
 from django.utils import timezone 
 from django_admin_listfilter_dropdown.filters import RelatedDropdownFilter, DropdownFilter
-
+from rangefilter.filters import DateRangeFilter, DateTimeRangeFilter
+from import_export.admin import ImportExportModelAdmin
+from products.resources import ReviewResource
 
 class ReviewInline(admin.TabularInline):  # StackedInline farklı bir görünüm aynı iş
     '''Tabular Inline View for '''
@@ -18,7 +20,7 @@ class ProductAdmin(admin.ModelAdmin):
     # readonly_fields = ("create_date",)
     list_display = ("name", "create_date", "is_in_stock", "update_date","added_days_ago","how_many_reviews", "bring_img_to_list")
     list_editable = ( "is_in_stock", )
-    list_filter = ("is_in_stock", "create_date", ("name", DropdownFilter))
+    list_filter = ("is_in_stock", ("create_date", DateTimeRangeFilter), ("name", DropdownFilter))
     list_display_links = ("name",)
     search_fields = ("name","create_date")
     prepopulated_fields = {'slug' : ('name',)}
@@ -68,6 +70,7 @@ class ReviewAdmin(admin.ModelAdmin):
     list_per_page = 50
     raw_id_fields = ('product',) 
     list_filter = (('product', RelatedDropdownFilter),)
+    resource_class = ReviewResource
 
 
 admin.site.register(Product,ProductAdmin)
